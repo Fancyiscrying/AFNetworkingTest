5d702b7a4b0e45651fdd43e6570413a3ca68ad2c
Fancyiscrying <Fancy@FancydeAir.mshome.net>
2016-02-24 15:03:06 +0800

    SmartPen 的借口测试

--- AFNetworkingTest/ViewController.m
+++ AFNetworkingTest/ViewController.m
@@ -5,11 +5,21 @@
 //  Created by Fancy on 16/2/24.
 //  Copyright © 2016年 Fancy. All rights reserved.
 //
-
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 #import "ViewController.h"
-
+#import "AFNetworking.h"
+#ifdef DEBUG
+#define posturl    @"http://10.244.171.106/SmartPen/user.json"
+#define geturl      @"http://10.244.171.106/SmartPen/securityQuestion.json"
+#else 
+#define posturl    @""
+#define geturl     @""
+#endif
 @interface ViewController ()
+- (IBAction)postJson:(id)sender;
 
+- (IBAction)getJson:(id)sender;
 @end
 
 @implementation ViewController
@@ -24,4 +34,35 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)postJson:(id)sender {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    
+    NSDictionary *dict =@{@"username":@"hehe",@"pwd":@"hahahah"};
+    
+    [manager POST:posturl parameters:dict success:^(AFHTTPRequestOperation *operation, id responseObject){
+    
+        NSLog(@"返回结果是：%@",responseObject);
+    } failure:^(AFHTTPRequestOperation *operation,NSError *error){
+        NSLog(@"%@",error);
+    }];
+    
+    
+    
+}
+
+- (IBAction)getJson:(id)sender {
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    
+    [manager GET:geturl parameters:nil success:^(AFHTTPRequestOperation *operation,id responseObject){
+         NSLog(@"%@",responseObject);
+     
+     
+     }failure:^(AFHTTPRequestOperation *operation ,NSError *error)
+    { NSLog(@"%@",error);
+    }];
+}
 @end
